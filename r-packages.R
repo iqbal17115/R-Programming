2afdb17fc825e9ac8ff05a56a06c19516dd47f5a
Md Iqbal Hossain <iqbal86571361@gmail.com>
2022-06-25 20:07:11 +0600

    R programming

---
+++ r-packages.R
@@ -0,0 +1,13 @@
+# Package install
+ install.packages("gapminder")
+ library(gapminder) 
+
+# Gapminder
+ head(gapminder)
+ dim(gapminder) 
+ str(gapminder) 
+ summary(gapminder)
+
+# Documentation
+ ?gapminder
+ 
